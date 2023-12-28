8e5a5cbf093371563146918b6d98881b865d6398
harith01 <harithyusuph@gmail.com>
2023-12-28 07:18:53 +0100

    Create Header component

--- src/App.js
+++ src/App.js
@@ -3,21 +3,25 @@ import { CssBaseline, Grid } from "@mui/material";
 import Header from "./components/Header/Header";
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme();
 
 function App() {
   return (
-    <>
-    <CssBaseline />
-    <Header />
-    <Grid container spacing={3} style={{ width: '100%'}}>
-      <Grid item xs={12} md={4}>
-        <List />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: '100%'}}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          <Map />
+        </Grid>
       </Grid>
-      <Grid item xs={12} md={8}>
-        <Map />
-      </Grid>
-    </Grid>
-    </>
+    </ThemeProvider>
+    
   );
 }
 
