3c7e73cd309f44e090dad27f43473790a8fb0598
harith01 <harithyusuph@gmail.com>
2024-01-01 11:32:29 +0100

    Create List component

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,52 @@
-import React from "react";
+import React, { useState} from "react";
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@mui/material";
+import useStyles from "./styles";
+
 
 const List = () => {
+    const classes = useStyles();
+    const [type, setType] = useState('restaurant');
+    const [rating, setRating] = useState('');
+    const places = [
+        {name: 'Kano'},
+        {name: 'Kaduna'},
+        {name: 'Katsina'}
+    ];
+
     return ( 
-        <h1>List</h1>
-     );
+        <div className={classes.container}>
+            <Typography variant="h4">
+                Restaurants, Hotels & Attractions around you
+            </Typography>
+
+            <FormControl className={classes.formControl}>
+                <InputLabel for="type">Type</InputLabel>
+                <Select id="type" value={type} onChange={(e) => setType(e.target.value )}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value )}>
+                    <MenuItem value={0}>All</MenuItem>
+                    <MenuItem value={3}>Above 3.0</MenuItem>
+                    <MenuItem value={4}>Above 4.0</MenuItem>
+                    <MenuItem value={4.5}>Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, i) => (
+                    <Grid item key={i} xs={12}>
+                       <Typography>{place.name}</Typography> 
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
+    );
 }
  
 export default List;
